59d6656e56693f26231290207cfd2dff9fe97c75
gjeanmart <greg@jeanmart.me>
2020-08-31 16:55:43 +0200

    Updates

---
+++ precache-manifest.b70bf713316154951ab2c441aac19b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cd84b7013b0563a9d50c445d7037df",
+    "url": "/portis-gsn-issue/index.html"
+  },
+  {
+    "revision": "3d28e4760ed2693902ba",
+    "url": "/portis-gsn-issue/static/js/2.24a149a5.chunk.js"
+  },
+  {
+    "revision": "3eea8a5929683a016eb0603b7448a52c",
+    "url": "/portis-gsn-issue/static/js/2.24a149a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c23193fcfb1583e7d3",
+    "url": "/portis-gsn-issue/static/js/main.d36057dc.chunk.js"
+  },
+  {
+    "revision": "aa17b7c28b31b23fe95e",
+    "url": "/portis-gsn-issue/static/js/runtime-main.852cc84d.js"
+  }
+]);
